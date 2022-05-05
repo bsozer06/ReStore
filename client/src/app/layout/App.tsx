ce948b55ec2912ffb8ef7d8ba29325318312c908
burhan sozer <you@example.com>
2022-05-05 20:27:39 +0300

    Added the switcher and updated Catalog component

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,40 +1,33 @@
-import { Container, CssBaseline } from '@mui/material';
-import React, { useEffect, useState } from 'react';
+import { Container, CssBaseline, createTheme, ThemeProvider } from '@mui/material';
+import { useState } from 'react';
 import Catalog from '../../features/catalog/Catalog';
-import { Product } from '../models/product';
 import Header from './Header';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false); 
+  const paletteType = darkMode ? "dark" : "light";
 
-  const [products, setProducts] = useState<Product[]>([]);
-
-  useEffect(() => {
-    fetch("http://localhost:5000/api/products")
-      .then(res => res.json())
-      .then(data => setProducts(data))
-  }, []);
-
-  function addProduct() {
-    setProducts(prevState => [...prevState,
-    {
-      id: 101 + prevState.length,
-      name: "product" + (prevState.length + 1),
-      price: (prevState.length * 100) + 100,
-      brand: "some brand",
-      description: "some description",
-      pictureUrl: "http://picsum.photos/200"
+  const theme = createTheme({
+    palette: {
+      mode: paletteType,
+      background: {
+        default: paletteType === 'light' ? "#eaeaea" : "#121212"
+      }
     }
-    ]);
+  })
+
+  function handleThemeChange() {
+    setDarkMode(!darkMode);
   }
 
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Header />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
-        <Catalog products={products} addProduct={addProduct} />
+        <Catalog />
       </Container>
-    </>
+    </ThemeProvider>
   );
 }
 
