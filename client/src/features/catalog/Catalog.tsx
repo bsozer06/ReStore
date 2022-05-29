3b3c145f985efe5f0aaacbd40dbb17a1f443a0b6
burhan sozer <you@example.com>
2022-05-29 16:05:10 +0300

    Set paginations of the catalog

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,5 +1,6 @@
 import { Box, Grid, Pagination, Paper, Typography } from '@mui/material';
 import { useEffect } from 'react';
+import AppPagination from '../../app/components/AppPagination';
 import CheckBoxButtons from '../../app/components/CheckBoxButtons';
 import RadioButtonGroup from '../../app/components/RadioButtonGroup';
 import LoadingComponent from '../../app/layout/LoadingComponent';
@@ -17,7 +18,7 @@ const sortOptions = [
 export default function Catalog() {
 
     const products = useAppSelector(productSelectors.selectAll)
-    const { productsLoaded, status, filtersLoaded, brands, types, productParams } = useAppSelector(state => state.catalog)
+    const { productsLoaded, status, filtersLoaded, brands, types, productParams, metaData } = useAppSelector(state => state.catalog)
     const dispatch = useAppDispatch()
 
     useEffect(() => {
@@ -28,7 +29,7 @@ export default function Catalog() {
         if (!productsLoaded) dispatch(fetchProductsAsync());
     }, [productsLoaded, dispatch]);
 
-    if (status.includes("pending")) return <LoadingComponent message='Loading Products...' />
+    if (status.includes("pending") || !metaData) return <LoadingComponent message='Loading Products...' />
 
     return (
         <Grid container spacing={4}>
@@ -64,17 +65,10 @@ export default function Catalog() {
 
             <Grid item xs={3} />
             <Grid item xs={9}>
-                <Box display="flex" justifyContent="space-between" alignItems="center">
-                    <Typography>
-                        Display 1-6 of 20 items
-                    </Typography>
-                    <Pagination
-                        color="secondary"
-                        size="large"
-                        count={10}
-                        page={2}
-                    />
-                </Box>
+                <AppPagination
+                    metaData={metaData}
+                    onPageChange={(page: number) => dispatch(setProductParams({ pageNumber: page }))}
+                />
             </Grid>
         </Grid>
     )
--- client/src/features/catalog/catalogSlice.ts
+++ client/src/features/catalog/catalogSlice.ts
@@ -1,5 +1,6 @@
 import { createAsyncThunk, createEntityAdapter, createSlice } from "@reduxjs/toolkit";
 import agent from "../../app/api/agent";
+import { MetaData } from "../../app/models/pagination";
 import { Product, ProductParams } from "../../app/models/product";
 import { RootState } from "../../app/store/configureStore";
 
@@ -10,6 +11,7 @@ interface CatalogState {
     brands: string[];
     types: string[];
     productParams: ProductParams;
+    metaData: MetaData | null;
 }
 
 const productsAdapter = createEntityAdapter<Product>()
@@ -31,7 +33,9 @@ export const fetchProductsAsync = createAsyncThunk<Product[], void, {state: Root
     async (_, thunkAPI) => {
         const params = getAxiosParams(thunkAPI.getState().catalog.productParams)
         try {
-            return await agent.Catalog.list(params);
+            const response = await agent.Catalog.list(params);
+            thunkAPI.dispatch(setMetaData(response.metaData));
+            return response.items;
         } catch (error: any) {
             thunkAPI.rejectWithValue({ error: error.data })
         }
@@ -76,7 +80,8 @@ export const catalogSlice = createSlice({
         status: "idle",
         brands: [],
         types: [],
-        productParams: initParams()
+        productParams: initParams(),
+        metaData: null
     }),
     reducers: {
         setProductParams: (state, action) => {
@@ -85,6 +90,9 @@ export const catalogSlice = createSlice({
         },
         resetProductParams: (state) => {
             state.productParams = initParams();
+        },
+        setMetaData: (state, action) => {
+            state.metaData = action.payload;
         }
     },
     extraReducers: (builder => {
@@ -129,4 +137,4 @@ export const catalogSlice = createSlice({
 
 
 export const productSelectors = productsAdapter.getSelectors((state: RootState) => state.catalog);
-export const { setProductParams, resetProductParams } = catalogSlice.actions;
+export const { setProductParams, resetProductParams, setMetaData } = catalogSlice.actions;
